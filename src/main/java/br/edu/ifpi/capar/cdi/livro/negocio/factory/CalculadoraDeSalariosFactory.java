f452e756e11d3a3e2a1fb2d0f33ec67865663110
Denylson Melo <denylsonmelo@gmail.com>
2016-12-04 21:18:22 -0300

    abordando o problema atraves do spring com arquivo de configuracao

--- src/main/java/br/edu/ifpi/capar/cdi/livro/negocio/factory/CalculadoraDeSalariosFactory.java
+++ src/main/java/br/edu/ifpi/capar/cdi/livro/negocio/factory/CalculadoraDeSalariosFactory.java
@@ -1,8 +1,8 @@
 package br.edu.ifpi.capar.cdi.livro.negocio.factory;
 
 import br.edu.ifpi.capar.cdi.livro.negocio.CalculadoraDeSalarios;
-import br.edu.ifpi.capar.cdi.livro.negocio.TabelaDeReferenciaSalarial;
-import java.util.ResourceBundle;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  *
@@ -10,22 +10,10 @@ import java.util.ResourceBundle;
  */
 public class CalculadoraDeSalariosFactory {
 
-    private final ResourceBundle bundle = ResourceBundle.getBundle("dependencias");
+    private final ApplicationContext context = new ClassPathXmlApplicationContext("dependencias.xml");
 
     public CalculadoraDeSalarios criaCalculadora() {
-        TabelaDeReferenciaSalarial pisosSalariais = criaInstancia(TabelaDeReferenciaSalarial.class);
-        CalculadoraDeSalarios calculadora = criaInstancia(CalculadoraDeSalarios.class);
-        calculadora.setTabelaDeReferenciaSalarial(pisosSalariais);
+        CalculadoraDeSalarios calculadora = (CalculadoraDeSalarios) context.getBean("calculadora");
         return calculadora;
-    }
-
-    private <T> T criaInstancia(Class<T> classe) {
-        String nomeDaClasse = bundle.getString(classe.getSimpleName());
-        try {
-            Class<?> clazz = Class.forName(nomeDaClasse);
-            return clazz.asSubclass(classe).newInstance();
-        } catch (ClassNotFoundException | IllegalAccessException | InstantiationException ex) {
-            return null;
-        }
     }
 }
